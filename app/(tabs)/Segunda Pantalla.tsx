6d1254fc6a7ef89748332ec40b59d3f60954176b
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-04-30 19:54:39 -0300

    Subir prueba

--- app/(tabs)/Segunda Pantalla.tsx
+++ app/(tabs)/Segunda Pantalla.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function App(){
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
-            <Text style={{color: 'white'}}>Hola</Text>
+            <Text style={{color: 'white'}}>Prueba</Text>
         </View>
     );
 }
