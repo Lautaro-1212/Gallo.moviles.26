4e652a3446a68b453d2d9c0cbfd3bd162de545dc
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-04-30 18:58:01 -0300

    Agregando trabajo 1, 2 y 3

---
+++ app/(tabs)/Segunda Pantalla.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App(){
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
+            <Text style={{color: 'white'}}>¡Segunda pantalla desde React Native!</Text>
+        </View>
+    );
+}
