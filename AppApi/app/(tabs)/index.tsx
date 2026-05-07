036f8cb255f642548b1baee0b5178de4d04261fd
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-05-07 19:50:26 -0300

    feat(index.jsx): crear pantalla inicial de useEffect

--- AppApi/app/(tabs)/index.tsx
+++ AppApi/app/(tabs)/index.tsx
@@ -7,5 +7,9 @@ export default function App() {
     console.log("La app se monto");
   },[])
 
-  
+  return(
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text style={{color:'white'}}>Usar useEffect</Text>
+    </View>
+  );
 }
