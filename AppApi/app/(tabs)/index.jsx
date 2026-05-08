4a971748986d0fc50d6236a826ac3f9eddc7cd2a
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-05-07 21:22:17 -0300

    refactor: separar lista de usuarios en un componente

--- AppApi/app/(tabs)/index.jsx
+++ AppApi/app/(tabs)/index.jsx
@@ -4,7 +4,7 @@ import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 export default function App() {
   const [usuarios, setUsuarios] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
@@ -22,32 +22,38 @@ export default function App() {
     return <ActivityIndicator size="large" color="green" />;
   }
 
+  function Lista({mostrar: CajasProps}){
+    return (
+      <View style={{ flex: 1, padding: 20}}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10, color:'white' }}>
+          Lista de usuarios
+        </Text>
+
+        <FlatList
+          data={usuarios}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                padding: 10,
+                marginBottom: 5,
+                backgroundColor: '#f2f2f2',
+                borderRadius: 10,
+              }}
+            >
+              <Text style={{ fontWeight: 'bold' }}>
+                {item.name}
+              </Text>
+
+              <Text>{item.email}</Text>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, padding: 20}}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>
-        Lista de usuarios
-      </Text>
-
-      <FlatList
-        data={usuarios}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => (
-          <View
-            style={{
-              padding: 10,
-              marginBottom: 5,
-              backgroundColor: '#f2f2f2',
-              borderRadius: 10,
-            }}
-          >
-            <Text style={{ fontWeight: 'bold' }}>
-              {item.name}
-            </Text>
-
-            <Text>{item.email}</Text>
-          </View>
-        )}
-      />
-    </View>
+    <Lista />
   );
 }
