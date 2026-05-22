b7f4b501c8b1b15c1c063db2e94e49a720e08bd5
Lauta1212 <lautarojoagallo.07@gmail.com>
2026-05-21 23:36:58 -0300

    chore(AppNavegation): Eliminar AppNavegation

--- AppNavegacion/src/app/pantalla.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Pantalla() {
-  return (
-    <View style={{flex:1, alignItems:'center', justifyContent:'center', backgroundColor:'black'}}>
-      <Text style={{color:'red', fontSize:200}}>EXPO ME CAGASTE LA VIDA</Text>
-    </View>
-  );
-}
