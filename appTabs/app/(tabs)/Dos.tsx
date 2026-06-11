4d1e9f7bbad740c3f26322c3431ea52efef6cd8d
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-06-11 19:04:40 -0300

    feat: agregar tamanio al texto

--- appTabs/app/(tabs)/Dos.tsx
+++ appTabs/app/(tabs)/Dos.tsx
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 export default function App(){
   return(
     <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
-      <Text style={{color: 'white'}}>Esto es una pestaña con tabs</Text>
+      <Text style={{color: 'white', fontSize:30}}>Esto es una pestaña con tabs</Text>
     </View>
   );
 }
